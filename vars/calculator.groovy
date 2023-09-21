76918fea5d0d0a30b7eafc8bad93a702a9fad525
kishansingh <ks774887@gmail.com>
2023-09-21 19:51:55 +0530

    new groovy file added

---
+++ vars/calculator.groovy
@@ -0,0 +1,11 @@
+def add(x,y){
+	echo "the sum of ${x} and ${y} is ${x+y}"
+}
+
+def mul(x,y){
+	echo "the sum of ${x} and ${y} is ${x*y}"
+}
+
+def call(){
+	echo "this is a HELLO from the call tag"
+}
