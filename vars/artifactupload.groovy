c305bc54e21f153393752ea567f6e893493ea0d2
kishansingh <ks774887@gmail.com>
2023-09-25 11:58:11 +0530

    added nexus path

---
+++ vars/artifactupload.groovy
@@ -0,0 +1,3 @@
+def call(repo){
+	sh 'nexusArtifactUploader artifacts: [[artifactId: 'my-web-app', classifier: '', file: 'target/my-web-app.war', type: 'war']], credentialsId: 'nexus-credentials', groupId: 'in.kishan', nexusUrl: '65.2.128.242:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'http://65.2.128.242:8081/repository/maven-snapshots/', version: '1.1-SNAPSHOT'
+}
